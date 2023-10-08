0711ecc692492358284aacf2c9da97523d286bee
zh <zhxhyy@qq.com>
2023-10-08 20:21:54 +0800

    Dijkstra

--- Dijkstra.py
+++ Dijkstra.py
@@ -3,26 +3,45 @@
 """
 
 import heapq
+import math
 
 graph = {
-  "A": [{"B":5}, {"C":1}],
-  "B": [{"A":5}, {"C":2}, {"D":1}],
-  "C": [{"A":1}, {"B":2}, {"D":4}, {"E":6}],
-  "D": [{"B":1}, {"C":4}, {"E":3}, {"F":8}],
-  "E": [{"C":8}, {"D":3}],
-  "F": [{"D":6}],
+  "A": {"B":5, "C":1},
+  "B": {"A":5, "C":2, "D":1},
+  "C": {"A":1, "B":2, "D":4, "E":8},
+  "D": {"B":1, "C":4, "E":3, "F":6},
+  "E": {"C":8, "D":3},
+  "F": {"D":6},
 }
+
+def init_distance(graph, s):
+  distance = {s: 0}
+  for vertex in graph:
+    if vertex != s:
+      distance[vertex] = math.inf
+  return distance
 
 def Dijkstra(graph, s):
   pqueue = []
-  seen = set()
   heapq.heappush(pqueue, (0, s))
+  seen = set()
+  parent = {s: None}
+  distance = init_distance(graph, s)
+
   while (len(pqueue) > 0):
     pair = heapq.heappop(pqueue)
     dist = pair[0]
     vertex = pair[1]
     seen.add(vertex)
+
     nodes = graph[vertex].keys()
     for w in nodes:
       if w not in seen:
-        
+        if dist + graph[vertex][w] < distance[w]:
+          heapq.heappush(pqueue, (dist + graph[vertex][w], w))
+          parent[w] = vertex
+          distance[w] = dist + graph[vertex][w]
+  return parent, distance
+
+parent, distance = Dijkstra(graph, 'A')
+print(parent, distance)
